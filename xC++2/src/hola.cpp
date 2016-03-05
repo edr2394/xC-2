83a5d39cddf1916ab9e3584473282231a2892e0a
Fabcast46 <fabcast46@gmail.com>
2016-03-05 04:38:49 -0600

    Added files via upload
    prueba

---
+++ xC++2/src/hola.cpp
@@ -0,0 +1,18 @@
+/*
+ * hola.cpp
+ *
+ *  Created on: 5 de mar. de 2016
+ *      Author: fabricio
+ */
+
+#include "hola.h"
+
+hola::hola() {
+	// TODO Auto-generated constructor stub
+
+}
+
+hola::~hola() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ xC++2/src/hola.h
@@ -0,0 +1,17 @@
+/*
+ * hola.h
+ *
+ *  Created on: 5 de mar. de 2016
+ *      Author: fabricio
+ */
+
+#ifndef HOLA_H_
+#define HOLA_H_
+
+class hola {
+public:
+	hola();
+	virtual ~hola();
+};
+
+#endif /* HOLA_H_ */
